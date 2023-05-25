733b9bb4d2b323fec4bb52e602bde5504d9f0ec9
maxyou <hyyou2010@gmail.com>
2023-05-26 02:12:17 +0800

    add rest api folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,24 @@
 'use client'
 
-import useSWR from 'swr'
-import { Fetcher } from "swr";
-import { ApiDef } from '../common'
+import Link from "next/link"
+
 
 export default function Home() {
 
-  const fetcher: Fetcher<ApiDef.User> = async (url:string) => {
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
-  };
-
-  const { data, error, isLoading } = useSWR('/api', fetcher)
- 
-  if (error) return <div>failed to load</div>
-  if (isLoading) return <div>loading...</div>
-
   return (
     <main className="w-full h-full flex flex-col items-center justify-between p-24">
-      <div className="flex flex-row items-center justify-between">
-          <input className="w-1/2 h-12 px-4 py-2 text-lg text-gray-700 placeholder-gray-500 border rounded-lg focus:shadow-outline" placeholder="input at here" />
-          <button className="px-4 py-2 ml-2 text-lg font-bold text-white bg-blue-500 rounded-lg hover:bg-blue-700">Submit</button>
+
+      <div>
+        <Link href="/rest">
+          <button className='bg-blue-500 min-w-fit hover:bg-blue-700 text-white p-2 m-2 rounded'>use restful api</button>
+        </Link>
+      </div>
+      <div>
+        <Link href="/graphql">
+          <button className='bg-blue-500 min-w-fit hover:bg-blue-700 text-white p-2 m-2 rounded'>use graphql api</button>
+        </Link>
       </div>
 
-      <div className="flex flex-col items-center justify-between">
-
-        <div>
-          <p className="text-2xl font-bold">{data?.name}</p>
-        </div>
-        <div>
-          <p className="text-2xl font-bold">Documentation 2</p>
-        </div>
-        <div>
-          <p className="text-2xl font-bold">Documentation 3</p>
-        </div>
-        <div>
-          <p className="text-2xl font-bold">Documentation 4</p>
-        </div>
-
-      </div>
     </main>
   )
 }
