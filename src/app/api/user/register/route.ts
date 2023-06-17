ee939812d1856ff0e9a8dfacdba0811879e9c6fa
maxyou <hyyou2010@gmail.com>
2023-06-18 02:40:06 +0800

    jwt.sign + cookie for response

--- src/app/api/user/register/route.ts
+++ src/app/api/user/register/route.ts
@@ -2,6 +2,8 @@ import { NextResponse, NextRequest } from 'next/server';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { PrismaClient } from '@prisma/client';
 import { Calc } from '@/common';
+import jwt from 'jsonwebtoken';
+
 
 const prisma = new PrismaClient();
  
@@ -21,31 +23,49 @@ export async function GET(request: Request) {
   return NextResponse.json({ todoItems });
 }
 export async function POST(request: Request) {
+
   console.log('req.method:', request.method)
   console.log('req.url', request.url)
   
-  const { name, password } = await request.json();
-  console.log(`POST name: ${name}, password: ${password}`);
+  const { name:nameRequest, password, email } = await request.json();
+  console.log(`POST name: ${nameRequest}, password: ${password}, email:${email}`);
 
   try {
     const ret = await prisma.user.create({
       data: {
-        name: name,
+        name: nameRequest,
         password: password,
-        // email: '',
+        email: email,
       }
     });
 
     console.log(`prisma.todoItem.create return: ${JSON.stringify(ret)}`) 
     
-    // Remove password from the response object
-    const { password: _, ...responseData } = ret;
+    const { id, name: nameDB } = ret as { id: string; name: string};
+    const responseData = { id, name:nameDB}
 
-    return NextResponse.json({ code: 0, message: 'success', data: responseData });
+    const secret = process.env.JWT_SECRET as string;
+    
+    const options: jwt.SignOptions = {
+      expiresIn: '1h',
+    };
+
+    const token = jwt.sign(responseData, secret, options);
+    console.log(token);
+
+    const res = NextResponse.json({ code: 0, message: 'success', data: responseData })
+
+    const cookieOptions = {
+      httpOnly: true,
+      secure: true
+    };
+    res.cookies.set('jwt', token, cookieOptions);
+
+    return res;
 
   } catch (error) {
     console.error('Error during user registration:', error);
-    return NextResponse.json({ code: -1, message: 'Failed to register user.', data: { name } });
+    return NextResponse.json({ code: -1, message: 'Failed to register user.'});
   }
 }
 
---
+++ src/app/user/server/page.tsx
@@ -0,0 +1,12 @@
+import { cookies } from 'next/headers'
+ 
+export default function Page() {
+  const cookieStore = cookies()
+//   const theme = cookieStore.get('theme')
+  return (
+    <div>
+        <h1>Server Side Rendering</h1>
+        <p>cookieStore: {JSON.stringify(cookieStore)}</p>
+    </div>
+  )
+}
