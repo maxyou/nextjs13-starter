3bc7dfb70beac72d34e43b43edd6a5fff79c9a03
maxyou <hyyou2010@gmail.com>
2023-06-21 04:40:16 +0800

    server side read cookies

--- src/app/biz/server/page.tsx
+++ src/app/biz/server/page.tsx
@@ -1,4 +1,4 @@
-import { headers } from 'next/headers'
+import { headers, cookies } from 'next/headers'
 import React from 'react'
 import ClientPage from './clientpage'
 
@@ -6,10 +6,16 @@ export default function Server() {
   const headersList = headers()
   const middlewareSet = headersList.get('middlewareSet')
 
+  const cookiesList = cookies()
+  const jwt = cookiesList.get('jwt')
+
   return (
     <div>
+      <br/>
         <p>middlewareSet: {JSON.stringify(middlewareSet)}</p>        
         <p>headersList: {JSON.stringify(headersList)}</p>
+        <p>jwt: {JSON.stringify(jwt)}</p>
+      <br/>
         <ClientPage middlewareSet={middlewareSet} />
     </div>
   )
