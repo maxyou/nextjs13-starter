f00e98ef465d08617c3a650078053a6b661e7ec2
maxyou <hyyou2010@gmail.com>
2023-06-22 10:10:20 +0800

    adjust ui

--- src/app/biz/todolist/todolist.tsx
+++ src/app/biz/todolist/todolist.tsx
@@ -45,10 +45,10 @@ const FETCH_TODO_ITEMS_QUERY = `
   }
 `;
 
-const TodoListPage: React.FC<{ jwtUser: JwtUser }> = ({jwtUser}) => {
+const TodoListPage: React.FC<{ jwtUser: JwtUser }> = ({ jwtUser }) => {
 
   const router = useRouter();
-  
+
   const userId = jwtUser.id;
   console.log('TodoListPage userId', userId);
 
@@ -70,40 +70,42 @@ const TodoListPage: React.FC<{ jwtUser: JwtUser }> = ({jwtUser}) => {
   });
 
   const handleAddTodo = () => {
-    addTodo({ content: newTodoContent, userId:jwtUser.id }).then(() => {
+    addTodo({ content: newTodoContent, userId: jwtUser.id }).then(() => {
       setNewTodoContent('');
       reexecuteQuery({ requestPolicy: 'network-only' });
     });
   };
 
-  const handleMarkTodoDone = (id: string, done:string) => {
+  const handleMarkTodoDone = (id: string, done: string) => {
     markTodoDone({ id, done }).then(result => {
       console.log('markTodoDone item and reexecuteQuery()', id);
       reexecuteQuery({ requestPolicy: 'network-only' });
     });
   };
 
-  const handleDeleteTodo = (id: string, content:string) => {
+  const handleDeleteTodo = (id: string, content: string) => {
     if (window.confirm(`delete: ${content}`)) {
 
       deleteTodo({ id }).then(result => {
         console.log('deleteTodo item and reexecuteQuery()', id);
         reexecuteQuery({ requestPolicy: 'network-only' });
       });
-          
+
     } else {
       // Do nothing!
       console.log('delete canceled');
     }
 
   };
+
   const refreshItems = () => {
     console.log('refreshItems');
     reexecuteQuery({ requestPolicy: 'network-only' });
   };
+
   const logout = () => {
     console.log('logout');
-    
+
     const url = "/api/user/logout";
     const options = {
       method: "POST",
@@ -115,14 +117,14 @@ const TodoListPage: React.FC<{ jwtUser: JwtUser }> = ({jwtUser}) => {
         name: jwtUser.name,
       }),
     };
-    
+
     console.log(`POST name: ${jwtUser.name}`);
 
     fetch(url, options)
       .then((response) => response.json())
-      .then((data) => {        
+      .then((data) => {
         console.log(data);
-        if (data.code === 0) {          
+        if (data.code === 0) {
           console.log(`Logout success: ${data.message}`);
 
           // redirect to todolist page
@@ -132,8 +134,8 @@ const TodoListPage: React.FC<{ jwtUser: JwtUser }> = ({jwtUser}) => {
           // const queryParams = encodeURIComponent(jsonString);
 
           router.push('/user/login');
-        }else{
-          
+        } else {
+
           console.log(`Logout failed: ${data.message}`);
         }
       });
@@ -141,25 +143,28 @@ const TodoListPage: React.FC<{ jwtUser: JwtUser }> = ({jwtUser}) => {
 
     // Perform registration logic here
     // You can send the data to an API or handle it as per your requirement
-    console.log('Login submitted');    
+    console.log('Login submitted');
 
   };
 
   return (
 
     <div className="container mx-auto p-4">
-        <button
+      <div className="flex mb-4 justify-end items-center">
+        user: {jwtUser.name}
+        {/* <button
           className="bg-blue-500 text-white p-2 rounded-md ml-2"
           onClick={refreshItems}
         >
           Refresh
-        </button>
+        </button> */}
         <button
           className="bg-blue-500 text-white p-2 rounded-md ml-2"
           onClick={logout}
         >
           Logout
         </button>
+      </div>
       <div className="flex mb-4">
         <input
           type="text"
@@ -193,7 +198,7 @@ const TodoListPage: React.FC<{ jwtUser: JwtUser }> = ({jwtUser}) => {
               {!todoItem.done && (
                 <button
                   className="bg-green-500 text-white p-2 rounded-md"
-                  onClick={() => handleMarkTodoDone(todoItem.id,'false')}
+                  onClick={() => handleMarkTodoDone(todoItem.id, 'false')}
                 >
                   Done
                 </button>
@@ -201,7 +206,7 @@ const TodoListPage: React.FC<{ jwtUser: JwtUser }> = ({jwtUser}) => {
               {todoItem.done && (
                 <button
                   className="bg-green-300 text-white p-2 rounded-md relative"
-                  onClick={() => handleMarkTodoDone(todoItem.id,'true')}
+                  onClick={() => handleMarkTodoDone(todoItem.id, 'true')}
                 >
                   Done
                   <span className="absolute inset-0 flex items-center justify-center">
