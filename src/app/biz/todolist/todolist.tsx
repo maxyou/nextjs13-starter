91680d47effbe06fafc7a9450748822177253c07
maxyou <hyyou2010@gmail.com>
2023-07-04 11:21:04 +0800

    avatar default svg

--- src/app/biz/todolist/todolist.tsx
+++ src/app/biz/todolist/todolist.tsx
@@ -5,6 +5,7 @@ import React, { useState } from 'react';
 import { useQuery, useMutation } from 'urql'
 import { useRouter } from 'next/navigation';
 import { Calc } from '@/common';
+import AvatarDefaut from '@/common/component/avatar-default';
 
 // GraphQL mutation to add a todo item
 const ADD_TODO_MUTATION = `
@@ -176,20 +177,7 @@ const TodoListPage: React.FC<{ jwtUser: JwtUser }> = ({ jwtUser }) => {
               className="w-8 h-8 rounded-full mr-2"
             />
           ) : (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="w-8 h-8 mr-2"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <circle cx="12" cy="12" r="10" />
-              <path d="M12 16v-2M12 8V6" />
-              <path d="M16 12h2M8 12h2" />
-            </svg>
+            <AvatarDefaut className="w-8 h-8 mr-2" />
           )}
           <p className="text-gray-700">{Calc.getShowNameFromJwtUser(jwtUser)}</p>
         </div>
